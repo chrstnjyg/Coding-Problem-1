c9209a93c44b869cf4650fd713254928ff590294
Christine <guerrerochristinejoy15@gmail.com>
2024-11-04 21:52:13 +0800

    Initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Christine Joy Guerrero";
+let age = "19";
+let favoriteNumber = "15";
+let favoriteColor = "Red";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
